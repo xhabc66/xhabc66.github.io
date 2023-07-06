f27b21a070f94b3fec07b6687bfb13dc95e3f836
xhabc66 <136218677+xhabc66@users.noreply.github.com>
2023-07-06 19:58:36 +0800

    Create add.js

---
+++ add.js
@@ -0,0 +1,3 @@
+addLoadEvent(function(){
+    document.getElementsByTagName('body')[0].innerHTML=document.getElementsByTagName('body')[0].innerHTML+'<a href="https://xhabc66.github.io/" style="position: absolute;left:0px;top:0px;"><<-回到主页</a>';
+})
