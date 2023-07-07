bd4f4358d6138909565c193af2f0235a77f02851
xhabc66 <136218677+xhabc66@users.noreply.github.com>
2023-07-07 18:39:03 +0800

    Update add.js

--- add.js
+++ add.js
@@ -1,3 +1,16 @@
+function addLoadEvent(event)
+{
+    if(typeof window.onload=='undefined')window.onload=event;
+    else
+    {
+        old=window.onload;
+        window.onload=function(){
+            old();
+            event();
+        }
+    }
+}
+
 addLoadEvent(function(){
     document.getElementsByTagName('body')[0].innerHTML=document.getElementsByTagName('body')[0].innerHTML+'<a href="https://xhabc66.github.io/" style="position: absolute;left:0px;top:0px;"><<-回到主页</a>';
 })
